aa399fb03886acd62efd0a3c2997099e634574d3
Clément <clement.salomon@etu.univ-amu.fr>
2024-04-01 14:54:00 +0200

    Structure du projet, test et début du style :)

---
+++ assets/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#696969',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
+
+export default styles;
